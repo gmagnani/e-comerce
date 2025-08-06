90e6a211765a70b443c03bacb4da8255e8e4face
Geovanni Magnani <magnanigeovanni@gmail.com>
2025-08-06 08:31:33 -0300

    Chore: split cards in separate components of sign in and sign up

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,20 +1,13 @@
-import { Button } from "@/components/ui/button"
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+
 import {
     Tabs,
     TabsContent,
     TabsList,
     TabsTrigger,
 } from "@/components/ui/tabs"
+
+import SignInForm from "./components/sign-in-form"
+import SignUpForm from "./components/sign-up-form"
 
 export function AuthenticationPage() {
     return (
@@ -25,54 +18,10 @@ export function AuthenticationPage() {
                     <TabsTrigger value="sign-up">Criar conta</TabsTrigger>
                 </TabsList>
                 <TabsContent value="sign-in">
-                    <Card>
-                        <CardHeader>
-                            <CardTitle>Entrar</CardTitle>
-                            <CardDescription>
-                                Faça login para continuar.
-                            </CardDescription>
-                        </CardHeader>
-                        <CardContent className="grid gap-6">
-                            <div className="grid gap-3">
-                                <Label htmlFor="email">Email</Label>
-                                <Input id="email" placeholder="Digite seu email" />
-                            </div>
-                            <div className="grid gap-3">
-                                <Label htmlFor="password">Senha</Label>
-                                <Input id="password" type="password" placeholder="Digite sua senha" />
-                            </div>
-                        </CardContent>
-                        <CardFooter>
-                            <Button>Entrar</Button>
-                        </CardFooter>
-                    </Card>
+                    <SignInForm />
                 </TabsContent>
                 <TabsContent value="sign-up">
-                    <Card>
-                        <CardHeader>
-                            <CardTitle>Criar conta</CardTitle>
-                            <CardDescription>
-                                Crie uma conta para continuar.
-                            </CardDescription>
-                        </CardHeader>
-                        <CardContent className="grid gap-6">
-                            <div className="grid gap-3">
-                                <Label htmlFor="name">Nome</Label>
-                                <Input id="name" placeholder="Digite seu nome" />
-                            </div>
-                            <div className="grid gap-3">
-                                <Label htmlFor="email">Email</Label>
-                                <Input id="email" placeholder="Digite seu email" />
-                            </div>
-                            <div className="grid gap-3">
-                                <Label htmlFor="password">Senha</Label>
-                                <Input id="password" type="password" placeholder="Digite sua senha" />
-                            </div>
-                        </CardContent>
-                        <CardFooter>
-                            <Button>Criar conta</Button>
-                        </CardFooter>
-                    </Card>
+                    <SignUpForm />
                 </TabsContent>
             </Tabs>
         </div>
