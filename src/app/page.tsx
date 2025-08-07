71f932e7511a07ebdd164ee88992b51689fb642d
Geovanni Magnani <magnanigeovanni@gmail.com>
2025-08-06 22:01:45 -0300

    Feat: add product list and product item components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,34 @@
-import { Header } from "@/components/common/header";
+import Image from "next/image";
 
-export default function Home() {
+import { Header } from "@/components/common/header";
+import ProductList from "@/components/common/product-list";
+import { db } from "@/db";
+
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
   return (
     <>
       <Header />
+      <div className=" space-y-6">
+        <div className="px-5">
+          <Image src="/banner-01.png" alt="Leve uma vida com estilo" width={0} height={0} sizes="100vw" className="w-full h-auto rounded-lg" />
+        </div>
+
+        <ProductList title="Mais vendidos" products={products} />
+
+        <div className="px-5">
+          <Image src="/banner-02.png" alt="Seja autêntico" width={0} height={0} sizes="100vw" className="w-full h-auto rounded-lg" />
+
+        </div>
+      </div>
     </>
   );
-}
+};
+
+export default Home;
+
+
